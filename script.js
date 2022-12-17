368a7461290a1e9e4bb9b1004504ace0d73faf55
paulbkim1 <paulbkim1@gmail.com>
2022-12-17 10:16:05 -0600

    Webpage

---
+++ script.js
@@ -0,0 +1,12 @@
+function removecookie(acceptcookie) {
+    var cookiebox = document.querySelector(acceptcookie);
+    cookiebox.remove();
+}
+
+function newsucculent(newimg) {
+    newimg.src = "./images3/assets/succulents-2.jpg"
+}
+
+function oldsucculent(newimg) {
+    newimg.src = "./images3/assets/succulents-1.jpg"
+}
